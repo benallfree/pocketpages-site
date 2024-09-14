2f87cd5d93fb36c1434f3c70677aeb389ec7ff6a
Ben Allfree <ben@benallfree.com>
2024-09-14 08:42:05 -0700

    wip

--- pb_hooks/pages/(splash)/+load.ts
+++
@@ -1,7 +0,0 @@
-import { PageDataLoaderFunc } from 'pocketpages'
-
-export const load: PageDataLoaderFunc = (context) => {
-  return {
-    version: require(`${__hooks}/../package.json`).version,
-  }
-}
